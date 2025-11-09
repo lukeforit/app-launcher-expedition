567e1b6cf1bcf35825cb66c12272d6c4c2607e27
lukeforit <ukasz1412@gmail.com>
2025-11-09 14:08:51 +0000

    Update theme

--- ui-core/src/main/java/me/lukeforit/launcher/uicore/ui/theme/Color.kt
+++ ui-core/src/main/java/me/lukeforit/launcher/uicore/ui/theme/Color.kt
@@ -9,3 +9,23 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val GoldGommagePaintress = Color(0xff927240)
+val GoldGommageMaelle = Color(0xffe7b242)
+val GreyGommageVerso = Color(0xffcfbfa6)
+val RedGommageSophie = Color(0xff751e22)
+val BlackCurator = Color(0xff1c1c1c)
+
+val RedHealthBar = Color(0xff75252d)
+
+val RedPicto = Color(0xffb71515)
+val CeladonPicto = Color(0xff90ebeb)
+val BeigePictoBorder = Color(0xffd8c39f)
+val DarkBrownPictoBackground = Color(0xff1b1a18)
+val ClaretPictoBackground = Color(0xff391e2f)
+
+val GoldFont = Color(0xffd1bb84)
+val BeigeFont = Color(0xffebe2d4)
+val PurpleFont = Color(0xff9c86bb)
+val WhiteFont = Color(0xfffbfaf8)
+val CeladonFont = Color(0xff89e3e1)
